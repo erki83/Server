644b46df638c5cbc6fcb357bf73b7522f3c5342e
Erki Ruus <erki.ruus@gmail.com>
2017-03-04 20:21:55 +0200

    Muudatused indexis

--- index.php
+++ index.php
@@ -9,11 +9,16 @@
     <div class="link">
         <a href="esimene.html" target="frame">Esimene Ülesanne</a>
 		<a href="tabel1.html" target="frame">Esimene tabel</a>
-		<a href="tabel2.html" target="frame">Imelik tabel</a>
+		<a href="tabel2.html" target="frame">Span tabel</a>
 		<a href="get.html" target="frame">GET meetod</a>
 		<a href="post.html" target="frame">POST meetod</a>
+		<a href="lisand.php" target="frame">Link PHP faili</a>
+        <a id="demo"></p>
+
     </div>
-	<iframe name="frame">  
+	<script type="text/javascript" src="java.js"></script>
+	<iframe name="frame">
     </iframe>
+
 </body>
 </html>
---
+++ java.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Eruus on 04.03.2017.
+ */
+<script>
+document.getElementById("demo").innerHTML = Date();
+</script>
