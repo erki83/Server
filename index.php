39fa25b1a558fa5d696c999a2522aa32993549dc
Erki Ruus <erki.ruus@gmail.com>
2017-03-05 02:06:35 +0200

    Kolmand kodutöö valmimise esimene etapp

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <title>Kodutööde nimekiri</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" src="java.js"></script>
 </head>
 <body>
     <div class="link">
@@ -12,11 +13,13 @@
 		<a href="tabel2.html" target="frame">Span tabel</a>
 		<a href="get.html" target="frame">GET meetod</a>
 		<a href="post.html" target="frame">POST meetod</a>
+		<a href="css-bot.html" target="frame">Robot</a>
+		<a href="stiilid.html" target="frame">CSS stiilid</a>
 		<a href="lisand.php" target="frame">Link PHP faili</a>
         <a id="demo"></p>
 
     </div>
-	<script type="text/javascript" src="java.js"></script>
+	
 	<iframe name="frame">
     </iframe>
 
